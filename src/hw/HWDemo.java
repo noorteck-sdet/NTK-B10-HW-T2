a8eaa31584b68c47f2ebad05649444ebc4440e5d
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-31 22:18:25 -0400

    refactoring code

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -7,27 +7,29 @@ public class HWDemo {
 	public static void main(String[] args) {
 		System.out.println("HW");
 	}
-	
+
 	public static void firstMethod() {
 		System.out.println("Hello Team, have a good week");
 	}
-	public void add(int a,int b) {
-		System.out.println(a+b);
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
 		System.out.println("zainab");
 
 	}
-	public void div(int a,int b) {
-		System.out.println(a/b);
+
+	public void div(int a, int b) {
+		System.out.println(a / b);
 		System.out.println("zainab alqazzaz");
 
-
 	}
+
 	public static void getName() {
 		System.out.println("My Name:Shahad Aldoori");
 	}
-	
-	public static void PersonalInfo () {
-		
+
+	public static void PersonalInfo() {
+
 		String Name = "Sami";
 		String Address = "USA";
 		int ZipCode = 200000000;
@@ -35,7 +37,8 @@ public class HWDemo {
 		System.out.println("Home Address: " + Address);
 		System.out.println("Home ZipCode: " + ZipCode);
 	}
-	public static void staffDatabase () {
+
+	public static void staffDatabase() {
 		String name = "Sami";
 		String position = "Regional Director";
 		double salary = 25000;
@@ -45,25 +48,25 @@ public class HWDemo {
 		System.out.println("Staff Salary: " + salary);
 		System.out.println("Staff ID: " + id);
 
-	
 	}
 
-	public void getStudents () {
-		
+	public void getStudents() {
+
 		String student1 = "John";
 		String student2 = "Sebastian";
 		String student3 = "Antonio";
-		
+
 	}
-		public void languages () {
-			
-			String course1 = "English";
-			String course2 = "Spanish";
-			String course3 = "Italian";
-			String course4 = "Chinese";
-			
-		}
-		
+
+	public void languages() {
+
+		String course1 = "English";
+		String course2 = "Spanish";
+		String course3 = "Italian";
+		String course4 = "Chinese";
+
+	}
+
 	/**
 	 * 
 	 * @param studentList
@@ -71,18 +74,12 @@ public class HWDemo {
 	 * @return true if name is in the studentList, otherwise return false
 	 */
 	public boolean isStudentListed(ArrayList<String> studentList, String name) {
-		
-		for(String student : studentList) {
-			if(student.equals(name)) {
+
+		for (String student : studentList) {
+			if (student.equals(name)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	}
-
-
-
-
-
-
+}
